36bc1d0a648d9ca25617c0e14d25b9dec462dd43
DougTag <douglassonntagcarvalho@gmail.com>
2026-01-10 18:47:04 -0300

    <IMP> OPAMP configuration

--- lib/OPAMP/src/OPAMP.c
+++ lib/OPAMP/src/OPAMP.c
@@ -2,16 +2,22 @@
 #include <stm32g431xx.h>
 
 void OPAMP1_Init(){
-    // PA1 -> VINP; PA2 e ADC1_IN3 -> VOUT
+    // Used for adjusting the signal gain
+    // PA1 -> VINP; ADC1_IN3 -> VOUT; VINM is internally connected to gain resistors
     RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
-    OPAMP1->CSR |= OPAMP_CSR_VMSEL_1;    // Modo PGA
-    OPAMP1->CSR &= ~OPAMP_CSR_PGGAIN;    // Ganho = +2
-    OPAMP1->CSR &= ~OPAMP_CSR_OPAMPINTEN;// Saída fica conectada ao pino PA2 e canal ADC1_IN3
-    OPAMP1->CSR |= OPAMP_CSR_OPAMPxEN;   // Liga o OPAMP
+    OPAMP1->CSR |= OPAMP_CSR_HIGHSPEEDEN_Msk; // High speed mode (better slew rate)
+    OPAMP1->CSR |= OPAMP_CSR_VMSEL_1;    // PGA mode
+    OPAMP1->CSR &= ~OPAMP_CSR_PGGAIN;    // Gain = +2. Will be changed by software to find best gain.
+    OPAMP1->CSR &= ~OPAMP_CSR_OPAMPINTEN;// Output connected to pin PA2 (that is connected to channel ADC1_IN3)
+    OPAMP1->CSR |= OPAMP_CSR_OPAMPxEN;   // Turn OPAMP on
 }
 
 void OPAMP2_Init(){
-    // PA7 -> VINP; PA6 -> VOUT; PA5 -> VINM
-    // Modo standard
-    OPAMP2->CSR |= OPAMP_CSR_OPAMPxEN;   // Liga o OPAMP
+    // Used for the Sallen-Key filter (active 2nd order with Butterworth response)
+    // PA7 -> VINP; PA6 -> VOUT; VINM is internally connected to VOUT
+    RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+    OPAMP2->CSR |= OPAMP_CSR_HIGHSPEEDEN_Msk; // High speed mode (better slew rate)
+    OPAMP2->CSR |= OPAMP_CSR_VMSEL; // Follower mode
+    OPAMP2->CSR &= ~OPAMP_CSR_OPAMPINTEN;// Output connected to pin PA6
+    OPAMP2->CSR |= OPAMP_CSR_OPAMPxEN;   // Turn OPAMP on
 }
